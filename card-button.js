46071b2196dcd9ba2ee2dc0a5f976fa4a7973653
Susanna Parolo <77389479+susannaparolo@users.noreply.github.com>
2022-09-11 21:43:52 +0100

    Create card-button.js
    Created button component for item card

---
+++ card-button.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet } from 'react-native';
+import {Card, Title , Paragraph } from 'react-native-paper';
+import CardButton from "./card-button";
+
+
+const CreateCard = (props) => {
+	
+	return(
+		
+		<Card style={styles.container}>
+            <Card.Content style={styles.cardTitle}>
+                <Title style={styles.titleStyle}>{props.title}</Title>
+            </Card.Content>
+            
+            <Card.Content>
+                <Paragraph style={styles.paragraphStyle}>{props.description}</Paragraph>
+            </Card.Content>
+            <CardButton buttonText='APPLY NOW'/>
+
+        </Card>
+		
+	)
+}
+
+
+const styles = StyleSheet.create({
+	container: {
+		alignContent:'center',
+		height: 'auto',
+        width: 280,
+        borderRadius: 5,
+        marginHorizontal: 10,
+        marginVertical: 6,
+        backgroundColor: '#e2daeb',
+	},
+    titleStyle: {
+        color: '#1f1135',
+        fontWeight: 'bold',
+        fontSize: 13,
+        marginVertical: -5,
+        lineHeight: 20,
+        marginBottom: 10
+    },
+    paragraphStyle: {
+        fontSize: 12
+    }
+})
+
+export default CreateCard;
