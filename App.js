53402db60b6e5aa774918fb50f9c08f515d61a17
Susanna Parolo <77389479+susannaparolo@users.noreply.github.com>
2022-09-11 20:22:03 +0100

    Update App.js
    Moved bottom-navigator to its separate component

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-import Home from "./src/screens/Home";
-import LearnMore from "./src/screens/LearnMore";
-import Courses from "./src/screens/Courses";
-import FAQs from "./src/screens/FAQs";
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { StyleSheet } from "react-native";
+import BottomNavigator from "./src/components/bottom-navigator";
 
-
-const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator barStyle={styles.bottomBarStyle}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Learn More" component={LearnMore} />
-        <Tab.Screen name="Courses" component={Courses} />
-        <Tab.Screen name="FAQs" component={FAQs} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <BottomNavigator/>
   );
 }
 
