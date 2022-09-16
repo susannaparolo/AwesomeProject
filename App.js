c93deacd13632db0f34a66d0a134f4a0a4763e4b
Susanna Parolo <77389479+susannaparolo@users.noreply.github.com>
2022-09-16 11:49:44 +0100

    Add login page, update Opportunities name

--- App.js
+++ App.js
@@ -4,9 +4,24 @@ import BottomNavigator from "./src/components/bottom-navigator";
 
 export default function App() {
 
+  // const initialState = {username: "No user is set yet"}
+
+  // function usersReducer(state, action){
+  //   switch (action.type) {
+  //     case "ADD_USER":
+  //       return state.username = {...state, username:action.payload}
+  //     case "REMOVE_USER":
+  //       return state
+  //     default:
+  //       return state
+  //   }
+  // }
+
+  // const store = createStore(usersReducer,initialState);
+
   return (
-    <BottomNavigator/>
-  );
+      <BottomNavigator/>
+    );
 }
 
 const styles = StyleSheet.create({
